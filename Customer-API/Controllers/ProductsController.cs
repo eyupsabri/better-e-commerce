8cf59beb7b178b7442c9ff038976e5135d64761d
eyupsabri <99139272+eyupsabri@users.noreply.github.com>
2023-09-13 02:18:18 +0300

    ...

---
+++ Customer-API/Controllers/ProductsController.cs
@@ -0,0 +1,32 @@
+﻿using Business;
+using Business.APIPaging;
+using Business.DTOs;
+using Business.Filter;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Customer_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : Controller
+    {
+        private IProductsService _productsService;
+
+       
+        public ProductsController(IProductsService productsService)
+        {
+            _productsService = productsService;
+        }
+
+        [HttpGet]
+        [ProducesResponseType(200, Type = typeof(Paging<ProductResponse>))]
+        public  IActionResult GetProductsByCategoryId([FromQuery] int categoryId, [FromQuery] int pageIndex = 0)
+        {
+            ProductFilter filter = new ProductFilter() { CategoryId = categoryId, sortAsc = true, sortBy= "productName" };
+            var myList = _productsService.GetProducts(filter, pageIndex);
+
+
+            return Ok(new Paging<ProductResponse>(myList));
+        }
+    }
+}
