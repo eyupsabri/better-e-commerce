d748cb0ce5ac67bd08a96a27319ae76a1cd892e9
eyupsabri <99139272+eyupsabri@users.noreply.github.com>
2023-10-01 16:06:19 +0300

    getting dummy menu item

---
+++ Customer-API/Model/DummyCat.cs
@@ -0,0 +1,12 @@
+﻿using System.Reflection;
+
+namespace Customer_API.Model
+{
+    public class DummyCat
+    {
+        public int? ParentMenuItemId { get; set; }
+        public string Title { get; set; }
+        public int Id { get; set; }
+        public string? Url { get; set; }
+    }
+}
