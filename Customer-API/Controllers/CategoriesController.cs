3d8d1c15154aaef3e06ad6c568634dade6d02e5c
eyupsabri <99139272+eyupsabri@users.noreply.github.com>
2023-10-30 22:35:33 +0300

    updated dummy cats

--- Customer-API/Controllers/CategoriesController.cs
+++ Customer-API/Controllers/CategoriesController.cs
@@ -45,62 +45,25 @@ namespace Customer_API.Controllers
         [ProducesResponseType(200, Type = typeof(List<DummyCat>))]
         public  ActionResult GetDummyCategories()
         {
-            List<DummyCat> cats = new List<DummyCat>();
+            
             DummyCat dum1 = new DummyCat() { Id = 1, Title = "birinci"};
             DummyCat dum2 = new DummyCat() { Id = 2, Title = "ikinci" };
             DummyCat dum3 = new DummyCat() { Id = 3, Title = "ucuncu" };
-            DummyCat dum22 = new DummyCat() { Id = 22, Title = "Birincinin 10", ParentMenuItemId = 1, Url = "cekoslavakya" };
+          
 
             DummyCat dum4 = new DummyCat() { Id = 4, Title = "Birincinin 1", ParentMenuItemId = 1, Url = "cekoslavakya" };
-            DummyCat dum5 = new DummyCat() { Id = 5, Title = "ikincinin 1", ParentMenuItemId = 2, Url = "cekoslavakya" };
-            DummyCat dum6 = new DummyCat() { Id = 6, Title = "Ucuncunun 1", ParentMenuItemId = 3, Url = "cekoslavakya" };
+            DummyCat dum5 = new DummyCat() { Id = 5, Title = "ikincinin 1", ParentMenuItemId = 2, Url = "pakistan" };
+            DummyCat dum6 = new DummyCat() { Id = 6, Title = "Ucuncunun 1", ParentMenuItemId = 3, Url = "usa" };
 
-            DummyCat dum7 = new DummyCat() { Id = 7, Title = "Birincinin 2", ParentMenuItemId = 1, Url = "cekoslavakya" };
-            DummyCat dum8 = new DummyCat() { Id = 8, Title = "Birincinin 3", ParentMenuItemId = 1, Url = "cekoslavakya" };
-            DummyCat dum9 = new DummyCat() { Id = 9, Title = "Birincinin 4", ParentMenuItemId = 1, Url = "cekoslavakya" };
+            DummyCat dum7 = new DummyCat() { Id = 7, Title = "Birincinin 2", ParentMenuItemId = 1, Url = "germany" };
+            DummyCat dum8 = new DummyCat() { Id = 8, Title = "Birincinin 3", ParentMenuItemId = 1, Url = "india" };
+            DummyCat dum9 = new DummyCat() { Id = 9, Title = "Birincinin 4", ParentMenuItemId = 1, Url = "france" };
 
-            DummyCat dum10 = new DummyCat() { Id = 10, Title = "ikincinin 2", ParentMenuItemId = 2, Url = "cekoslavakya" };
-            DummyCat dum11 = new DummyCat() { Id = 11, Title = "ucuncunun 2", ParentMenuItemId = 3, Url = "cekoslavakya" };
-            DummyCat dum12 = new DummyCat() { Id = 12, Title = "ikincinin 3", ParentMenuItemId = 2, Url = "cekoslavakya" };
+            DummyCat dum10 = new DummyCat() { Id = 10, Title = "ikincinin 2", ParentMenuItemId = 2, Url = "egypt" };
+            DummyCat dum11 = new DummyCat() { Id = 11, Title = "ucuncunun 2", ParentMenuItemId = 3, Url = "italy" };
+            DummyCat dum12 = new DummyCat() { Id = 12, Title = "ikincinin 3", ParentMenuItemId = 2, Url = "finland" };
+            List<DummyCat> cats = new List<DummyCat>() { dum1, dum2, dum3, dum4, dum5, dum6, dum7, dum8, dum9, dum10, dum11, dum12};
 
-
-            DummyCat dum13 = new DummyCat() { Id = 13, Title = "Birincinin 1", ParentMenuItemId = 1, Url = "cekoslavakya" };
-            DummyCat dum14 = new DummyCat() { Id = 14, Title = "ikincinin 1", ParentMenuItemId = 2, Url = "cekoslavakya" };
-            DummyCat dum15 = new DummyCat() { Id = 15, Title = "Ucuncunun 1", ParentMenuItemId = 3, Url = "cekoslavakya" };
-
-            DummyCat dum16 = new DummyCat() { Id = 16, Title = "Birincinin 2", ParentMenuItemId = 1, Url = "cekoslavakya" };
-            DummyCat dum17 = new DummyCat() { Id = 17, Title = "Birincinin 3", ParentMenuItemId = 1, Url = "cekoslavakya" };
-            DummyCat dum18 = new DummyCat() { Id = 18, Title = "Birincinin 4", ParentMenuItemId = 1, Url = "cekoslavakya" };
-
-            DummyCat dum19 = new DummyCat() { Id = 19, Title = "ikincinin 2", ParentMenuItemId = 2, Url = "cekoslavakya" };
-            DummyCat dum20 = new DummyCat() { Id = 20, Title = "ucuncunun 2", ParentMenuItemId = 3, Url = "cekoslavakya" };
-            DummyCat dum21 = new DummyCat() { Id = 21, Title = "ikincinin 3", ParentMenuItemId = 2, Url = "cekoslavakya" };
-
-
-
-
-            cats.Add(dum1);
-            cats.Add(dum2);
-            cats.Add(dum3);
-            cats.Add(dum4);
-            cats.Add(dum5);
-            cats.Add(dum6);
-            cats.Add(dum7);
-            cats.Add(dum8);
-            cats.Add(dum9);
-            cats.Add(dum10);
-            cats.Add(dum11);
-            cats.Add(dum12);
-            cats.Add(dum13);
-            cats.Add(dum14);
-            cats.Add(dum15);
-            cats.Add(dum16);
-            cats.Add(dum17);
-            cats.Add(dum18);
-            cats.Add(dum19);
-            cats.Add(dum20);
-            cats.Add(dum21);
-            cats.Add(dum22);
 
             return Ok(cats);
         }
